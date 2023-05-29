080963eab6b2bb6a62da0b58ac98f9cc4a011409
Tselmeg ADIYAMUNKH <tadiyamu@e3r6p17.clusters.42paris.fr>
2023-05-29 20:32:17 +0200

    Print mutex

---
+++ philo/main.c
@@ -0,0 +1,109 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tadiyamu <tadiyamu@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/05/23 21:05:34 by tadiyamu          #+#    #+#             */
+/*   Updated: 2023/05/29 20:04:20 by tadiyamu         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "philo.h"
+
+long long	ft_get_now(void)
+{
+	struct timeval	tv;
+
+	gettimeofday(&tv, NULL);
+	return ((long long) tv.tv_sec * 1000LL + (long long) tv.tv_usec / 1000LL);
+}
+
+static int	ft_init_thread_data(t_data *data, int n, t_philo_config *config)
+{
+	int		i;
+
+	i = 0;
+	while (i < n)
+	{
+		memset(&data[i].fork_data.mutex, 0, sizeof(pthread_mutex_t));
+		if (pthread_mutex_init(&data[i].fork_data.mutex, NULL) != 0)
+			return (0);
+		data[i].config = config;
+		data[i].id = i + 1;
+		data[i].fork_data.fork = 1;
+		data[i].fork_data.time = config->now;
+		if (i + 1 < n)
+			data[i].next = &data[i + 1];
+		else
+			data[i].next = &data[0];
+		i++;
+	}
+	return (1);
+}
+
+static int	ft_init_config(t_philo_config *config, int argc, char **argv)
+{
+	if (ft_philo_parse(argc, argv, config))
+	{
+		config->now = ft_get_now();
+		config->ate_lock.stop_flag = 0;
+		config->time_lock.now = config->now;
+		config->time_lock.n = malloc(sizeof(int) * config->count);
+		if (config->time_lock.n == NULL)
+			return (0);
+		config->ate_lock.ate = malloc(sizeof(int) * config->count);
+		if (config->ate_lock.ate == NULL)
+		{
+			free(config->time_lock.n);
+			return (0);
+		}
+		memset(config->ate_lock.ate, 0, sizeof(int) * config->count);
+		memset(config->time_lock.n, 0, sizeof(int) * config->count);
+		memset(&config->time_lock.mutex, 0, sizeof(pthread_mutex_t));
+		memset(&config->ate_lock.mutex, 0, sizeof(pthread_mutex_t));
+		memset(&config->print_lock.mutex, 0, sizeof(pthread_mutex_t));
+		pthread_mutex_init(&config->time_lock.mutex, NULL);
+		pthread_mutex_init(&config->ate_lock.mutex, NULL);
+		pthread_mutex_init(&config->print_lock.mutex, NULL);
+		return (1);
+	}
+	return (0);
+}
+
+int	main(int argc, char **argv)
+{
+	pthread_t		*threads;
+	t_philo_config	config;
+	t_data			*data;
+	int				i;
+
+	if (!ft_init_config(&config, argc, argv))
+	{
+		write(2, "error\n", 6);
+		return (1);
+	}
+	data = malloc(sizeof(t_data) * config.count);
+	threads = malloc(sizeof(pthread_t) * config.count);
+	if (ft_init_thread_data(data, config.count, &config))
+	{
+		i = -1;
+		while (++i < config.count)
+			pthread_create(&threads[i], NULL, ft_loop_thread, &data[i]);
+	}
+	i = -1;
+	while (++i < config.count)
+		pthread_join(threads[i], NULL);
+	i = -1;
+	while (++i < config.count)
+		pthread_mutex_destroy(&data[i].fork_data.mutex);
+	pthread_mutex_destroy(&config.time_lock.mutex);
+	pthread_mutex_destroy(&config.ate_lock.mutex);
+	pthread_mutex_destroy(&config.print_lock.mutex);
+	free(config.ate_lock.ate);
+	free(config.time_lock.n);
+	free(data);
+	free(threads);
+	return (0);
+}
---
+++ philo/philo_actions.c
@@ -0,0 +1,82 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philo_actions.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tadiyamu <tadiyamu@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/05/25 21:10:12 by tadiyamu          #+#    #+#             */
+/*   Updated: 2023/05/29 20:07:09 by tadiyamu         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "philo.h"
+
+void	ft_philo_eat(t_data *data, t_thread_config *config)
+{
+	ft_philo_print(config->now, data->id, PHILO_FORK, data);
+	data->fork_data.fork = 0;
+	data->next->fork_data.fork = 0;
+	config->die_time = config->now + config->die_duration;
+	config->state = 2;
+	config->state_time = config->now + config->eat_duration;
+	ft_philo_print(config->now, data->id, PHILO_EAT, data);
+}
+
+void	ft_philo_sleep(t_data *data, t_thread_config *config)
+{
+	data->fork_data.fork = 1;
+	data->fork_data.time = config->now + 1;
+	data->next->fork_data.fork = 1;
+	data->next->fork_data.time = config->now + 1;
+	config->state = 3;
+	config->state_time = config->now + 1 + config->sleep_duration;
+	if (config->should_eat != -1 && config->should_eat != 0)
+		config->should_eat--;
+	ft_philo_print(config->now + 1, data->id, PHILO_SLEEP, data);
+}
+
+void	ft_philo_decide(t_data *data, t_thread_config *config)
+{
+	if (config->state == 3 && config->now == config->state_time)
+	{
+		if (data->fork_data.fork && data->next->fork_data.fork
+			&& config->now == data->fork_data.time
+			&& config->now == data->next->fork_data.time)
+			ft_philo_eat(data, config);
+		else
+		{
+			config->state = 1;
+			ft_philo_print(config->now, data->id, PHILO_THINK, data);
+		}
+	}
+}
+
+void	ft_philo_die(t_data *data, t_thread_config *config)
+{
+	int	i;
+	int	f;
+
+	if (config->die_time == config->now
+		&& pthread_mutex_lock(&data->config->ate_lock.mutex) == 0)
+	{
+		ft_philo_print(config->now, data->id, PHILO_DIE, data);
+		data->config->ate_lock.stop_flag = 1;
+		pthread_mutex_unlock(&data->config->ate_lock.mutex);
+	}
+	if (config->should_eat == 0
+		&& pthread_mutex_lock(&data->config->ate_lock.mutex) == 0)
+	{
+		data->config->ate_lock.ate[data->id - 1] = 1;
+		i = -1;
+		f = 0;
+		while (++i < data->config->count)
+		{
+			if (data->config->ate_lock.ate[i] == 0)
+				f = 1;
+		}
+		if (f == 0)
+			data->config->ate_lock.stop_flag = 1;
+		pthread_mutex_unlock(&data->config->ate_lock.mutex);
+	}
+}
